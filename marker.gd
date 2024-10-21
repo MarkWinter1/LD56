abc86a273c7f948892ee8a720c80449e57ed2bb6
MarkWinter1 <marknwinter01@gmail.com>
2024-10-20 19:21:41 -0700

    attempting to solve godot hardcrash?

--- marker.gd
+++ marker.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
-var current_workers = []
+var current_workers = [ null ]
 var marker_type = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("dont crash challenge")
 	pass
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	print("dont crash challenge 2 extra hard esition")
 	if Scents.time % 10 == 0:
 		var i = 0
 		while i < len(current_workers):
